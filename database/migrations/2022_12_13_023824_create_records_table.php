89117236483d6ec71ebf9ed0ad2db95bcff20c44
EC2 Default User <ec2-user@ip-172-31-25-53.ap-northeast-1.compute.internal>
2022-12-17 09:56:01 +0900

    前準備

---
+++ database/migrations/2022_12_13_023824_create_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->time('studied_time');
+            $table->text('subject');
+            $table->text('comment');
+            $table->date('studied_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+};
