d043a6aadb7bfa0bc52b03d3a75de7546b66098a
EC2 Default User <ec2-user@ip-172-31-25-53.ap-northeast-1.compute.internal>
2023-01-10 16:52:19 +0900

    css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,16 +3,31 @@
 <meta charset="utf-8">
 <head>
     <title>LOGIN</title>
+    <link rel="stylesheet" href="/css/login.css">
 </head>
+<div class="main_wrap">
+<header class="site-header">
+        <div class="wrapper site-header__wrapper">
+          <a href="#" class="brand">ポモドーロ・テクニックタイマー</a>
+          <nav class="nav">
+            <button class="nav__toggle" aria-expanded="false" type="button">
+              menu
+            </button>
+            <ul class="nav__wrapper">
+            </ul>
+          </nav>
+        </div>
+</header>
 <body>
-  <div>
-      <a href = "{{route('register')}}" >登録</a>
-  </div>
-  <div>
-      <a href = "{{route('login')}}" >ログイン</a>
-  </div>
-  <div>
-      <a href = "{{ url('/index') }}" >TOP</a>
+    <div class="register">
+        <a href = "{{route('register')}}" >新規登録</a>
+    </div>
+    <div class="login">
+        <a href = "{{route('login')}}" >ログイン</a>
+    </div>
+    <div class ="top">
+        <a href = "{{ url('/index') }}" >TOP</a>
 </div>
 </body>
+</div>
 </html>
